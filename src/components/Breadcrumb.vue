94582def058dfc19d4c1bc79eff9f03a2dfe0c2e
Jhonnatan Castro <jhonnatan.castro@orbis.com.pe>
2017-04-27 14:57:12 -0500

    refacto(components): funcionalidad basica

---
+++ src/components/Breadcrumb.vue
@@ -0,0 +1,24 @@
+<template>
+	<ul>
+		<li v-for="bread in breadcrumb"> {{bread}} </li>
+	</ul>
+</template>
+
+<script type="text/javascript">
+	export default{		
+		props: ["breadcrumb"]		
+	}
+</script>
+
+<style scope lang="stylus">
+	ul
+		padding 0
+		
+		li
+			display inline-block
+			
+			&:after
+				content "/"
+				display inline-block				
+	
+</style>
--- src/views/concesionario/mis-avisos/AvisosActivos.vue
+++ src/views/concesionario/mis-avisos/AvisosActivos.vue
@@ -1,9 +1,38 @@
-<template>
-	<h1>Avisos Activos</h1>
-	<!-- <breadcrumb
-		:param1=""
-	></breadcrumb>
-	<list-news></list-news> -->
+<template>	
+	<div>
+		<h1>Avisos Activos</h1>		
+		<breadcrumb :breadcrumb="BreadcrumbAvisosActivos"></breadcrumb>
+	</div>
+
 </template>
+
 <script type="text/javascript">
+	
+	import Breadcrumb from './../../../components/Breadcrumb.vue'	
+	import axios from 'axios'	
+
+	let urlBreadcrumbs 	 = 'http://www.json-generator.com/api/json/get/cvGPhPaniq?indent=2';
+	let urlAvisosActivos = 'https://randomuser.me/api/?results=20';
+
+	export default {
+		components : {Breadcrumb},		
+		mounted(){
+			this.loadBreadcrumb();
+			this.loadAvisosActivos();
+		},
+		methods : {
+			loadBreadcrumb(){
+				axios.get(urlBreadcrumbs)
+					.then((response)=>{					  
+					  this.BreadcrumbAvisosActivos = response.data.tags;	  
+					})
+			}			
+		},
+		data(){
+			return {
+				BreadcrumbAvisosActivos: []	
+			}
+		}
+	}
+
 </script>
