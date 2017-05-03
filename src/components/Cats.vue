0ab8be1fb78edf353369e62f9e1469927db5d9bf
Jhonnatan Castro <jhonnatan.castro@orbis.com.pe>
2017-05-03 17:15:06 -0500

    fix merge

---
+++ src/components/Cats.vue
@@ -0,0 +1,20 @@
+<template lang="pug">
+    div
+        ul
+            li(v-for="cat in data") {{cat}}        
+</template>
+
+<script type="text/javascript">
+    export default{		
+        props: ["data"]        
+    }
+</script>
+
+<style scope lang="stylus">
+    ul
+        padding 0
+        
+        li
+            display inline-block            
+    
+</style>
